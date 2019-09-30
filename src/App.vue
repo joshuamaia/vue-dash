b5ffa703f0124f5a7315fff81db218b14c730688
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-09-30 09:03:07 -0300

    Projeto base

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,55 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-app id="inspire" dark>
+      <v-navigation-drawer clipped fixed v-model="drawer" app>
+        <v-list dense>
+          <v-list-tile @click="navigateTo('home')">
+            <v-list-tile-action>
+              <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="navigateTo('about')">
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>About</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app fixed clipped-left>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <v-flex shrink>
+              <router-view />
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer app fixed>
+        <span>&copy; 2017</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return { drawer: true };
+  },
+  methods: {
+    navigateTo(where) {
+      this.$router.push({ name: where });
     }
   }
-}
-</style>
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-layout row>
+      <v-flex xs12 sm12>
+        <v-card>
+          <v-toolbar color="indigo" dark>
+            <v-toolbar-title>Habilidades</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-list>
+            <v-list-tile v-for="item in items" :key="item.title" avatar @click>
+              <v-list-tile-action>
+                <v-icon v-if="item.icon" color="pink">star</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-text="item.title"></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Desenvolvedor FullStack" },
+        {
+          title:
+            "Experiência com frontend web com ReactJS, Angular 7, JSP, JSF/Primefaces"
+        },
+        { title: "Experiência Mobile com React Native" },
+        {
+          title:
+            " Experiência com Backend Web com Node, Spring Boot, Java, JDBC e JPA/Hibernate"
+        },
+        { title: "Experiência com RESTful Web Service e SOAP" },
+        {
+          title: "Experiência com relatórios utilizando o Jasper Studio"
+        },
+        { title: "Experiência com GIT, Maven, Yarn e Npm" },
+        {
+          title:
+            "IDEs utilizadas para desenvolvimento Eclipse, Netbeans e VS Code"
+        },
+        {
+          title: "Banco de dados utilizados Mysql, Sql Server e Postgres"
+        }
+      ]
+    };
+  }
+};
+</script>
